891969d2c25497fbba2cfafd94bfa3c2a1995658
Aron Dobos <aron.dobos@outlook.com>
2015-07-07 15:52:51 +0000

    more cleanup/consolidation in LK
    git-svn-id: https://efmsvn2.nrel.gov/lk/svn/trunk@142 beec71b5-fff0-e442-8ac2-4f84a11f714e

--- lk_stdlib.cpp
+++ lk_stdlib.cpp
@@ -8,7 +8,6 @@
 #include <math.h>
 #include <float.h>
 
-#include "lk_math.h"
 #include "lk_stdlib.h"
 
 
@@ -2653,6 +2652,404 @@ bool lk::tex_doc( const lk_string &file,
 	fclose(fp);
 	return true;
 }
+
+double lk::besj0(double x)
+{
+	double ax,z;
+	double xx,y,ans,ans1,ans2;
+
+	if ((ax=fabs(x)) < 8.0) {
+		y=x*x;
+		ans1=57568490574.0+y*(-13362590354.0+y*(651619640.7
+			+y*(-11214424.18+y*(77392.33017+y*(-184.9052456)))));
+		ans2=57568490411.0+y*(1029532985.0+y*(9494680.718
+			+y*(59272.64853+y*(267.8532712+y*1.0))));
+		ans=ans1/ans2;
+	} else {
+		z=8.0/ax;
+		y=z*z;
+		xx=ax-0.785398164;
+		ans1=1.0+y*(-0.1098628627e-2+y*(0.2734510407e-4
+			+y*(-0.2073370639e-5+y*0.2093887211e-6)));
+		ans2 = -0.1562499995e-1+y*(0.1430488765e-3
+			+y*(-0.6911147651e-5+y*(0.7621095161e-6
+			-y*0.934945152e-7)));
+		ans=sqrt(0.636619772/ax)*(cos(xx)*ans1-z*sin(xx)*ans2);
+	}
+	return ans;
+}
+
+double lk::besj1(double x)
+{
+	double ax,z;
+	double xx,y,ans,ans1,ans2;
+
+	if ((ax=fabs(x)) < 8.0) {
+		y=x*x;
+		ans1=x*(72362614232.0+y*(-7895059235.0+y*(242396853.1
+			+y*(-2972611.439+y*(15704.48260+y*(-30.16036606))))));
+		ans2=144725228442.0+y*(2300535178.0+y*(18583304.74
+			+y*(99447.43394+y*(376.9991397+y*1.0))));
+		ans=ans1/ans2;
+	} else {
+		z=8.0/ax;
+		y=z*z;
+		xx=ax-2.356194491;
+		ans1=1.0+y*(0.183105e-2+y*(-0.3516396496e-4
+			+y*(0.2457520174e-5+y*(-0.240337019e-6))));
+		ans2=0.04687499995+y*(-0.2002690873e-3
+			+y*(0.8449199096e-5+y*(-0.88228987e-6
+			+y*0.105787412e-6)));
+		ans=sqrt(0.636619772/ax)*(cos(xx)*ans1-z*sin(xx)*ans2);
+		if (x < 0.0) ans = -ans;
+	}
+	return ans;
+}
+
+double lk::besy0(double x)
+{
+	double besj0(double x);
+	double z;
+	double xx,y,ans,ans1,ans2;
+
+	if (x < 8.0) {
+		y=x*x;
+		ans1 = -2957821389.0+y*(7062834065.0+y*(-512359803.6
+			+y*(10879881.29+y*(-86327.92757+y*228.4622733))));
+		ans2=40076544269.0+y*(745249964.8+y*(7189466.438
+			+y*(47447.26470+y*(226.1030244+y*1.0))));
+		ans=(ans1/ans2)+0.636619772*besj0(x)*log(x);
+	} else {
+		z=8.0/x;
+		y=z*z;
+		xx=x-0.785398164;
+		ans1=1.0+y*(-0.1098628627e-2+y*(0.2734510407e-4
+			+y*(-0.2073370639e-5+y*0.2093887211e-6)));
+		ans2 = -0.1562499995e-1+y*(0.1430488765e-3
+			+y*(-0.6911147651e-5+y*(0.7621095161e-6
+			+y*(-0.934945152e-7))));
+		ans=sqrt(0.636619772/x)*(sin(xx)*ans1+z*cos(xx)*ans2);
+	}
+	return ans;
+}
+
+double lk::besy1(double x)
+{
+	double besj1(double x);
+	double z;
+	double xx,y,ans,ans1,ans2;
+
+	if (x < 8.0) {
+		y=x*x;
+		ans1=x*(-0.4900604943e13+y*(0.1275274390e13
+			+y*(-0.5153438139e11+y*(0.7349264551e9
+			+y*(-0.4237922726e7+y*0.8511937935e4)))));
+		ans2=0.2499580570e14+y*(0.4244419664e12
+			+y*(0.3733650367e10+y*(0.2245904002e8
+			+y*(0.1020426050e6+y*(0.3549632885e3+y)))));
+		ans=(ans1/ans2)+0.636619772*(besj1(x)*log(x)-1.0/x);
+	} else {
+		z=8.0/x;
+		y=z*z;
+		xx=x-2.356194491;
+		ans1=1.0+y*(0.183105e-2+y*(-0.3516396496e-4
+			+y*(0.2457520174e-5+y*(-0.240337019e-6))));
+		ans2=0.04687499995+y*(-0.2002690873e-3
+			+y*(0.8449199096e-5+y*(-0.88228987e-6
+			+y*0.105787412e-6)));
+		ans=sqrt(0.636619772/x)*(sin(xx)*ans1+z*cos(xx)*ans2);
+	}
+	return ans;
+}
+
+
+double lk::besi0(double x)
+{
+	double ax,ans;
+	double y;
+
+	if ((ax=fabs(x)) < 3.75) {
+		y=x/3.75;
+		y*=y;
+		ans=1.0+y*(3.5156229+y*(3.0899424+y*(1.2067492
+			+y*(0.2659732+y*(0.360768e-1+y*0.45813e-2)))));
+	} else {
+		y=3.75/ax;
+		ans=(exp(ax)/sqrt(ax))*(0.39894228+y*(0.1328592e-1
+			+y*(0.225319e-2+y*(-0.157565e-2+y*(0.916281e-2
+			+y*(-0.2057706e-1+y*(0.2635537e-1+y*(-0.1647633e-1
+			+y*0.392377e-2))))))));
+	}
+	return ans;
+}
+
+double lk::besk0(double x)
+{
+	double besi0(double x);
+	double y,ans;
+
+	if (x <= 2.0) {
+		y=x*x/4.0;
+		ans=(-log(x/2.0)*besi0(x))+(-0.57721566+y*(0.42278420
+			+y*(0.23069756+y*(0.3488590e-1+y*(0.262698e-2
+			+y*(0.10750e-3+y*0.74e-5))))));
+	} else {
+		y=2.0/x;
+		ans=(exp(-x)/sqrt(x))*(1.25331414+y*(-0.7832358e-1
+			+y*(0.2189568e-1+y*(-0.1062446e-1+y*(0.587872e-2
+			+y*(-0.251540e-2+y*0.53208e-3))))));
+	}
+	return ans;
+}
+
+double lk::besi1(double x)
+{
+	double ax,ans;
+	double y;
+
+	if ((ax=fabs(x)) < 3.75) {
+		y=x/3.75;
+		y*=y;
+		ans=ax*(0.5+y*(0.87890594+y*(0.51498869+y*(0.15084934
+			+y*(0.2658733e-1+y*(0.301532e-2+y*0.32411e-3))))));
+	} else {
+		y=3.75/ax;
+		ans=0.2282967e-1+y*(-0.2895312e-1+y*(0.1787654e-1
+			-y*0.420059e-2));
+		ans=0.39894228+y*(-0.3988024e-1+y*(-0.362018e-2
+			+y*(0.163801e-2+y*(-0.1031555e-1+y*ans))));
+		ans *= (exp(ax)/sqrt(ax));
+	}
+	return x < 0.0 ? -ans : ans;
+}
+
+double lk::besk1(double x)
+{
+	double besi1(double x);
+	double y,ans;
+
+	if (x <= 2.0) {
+		y=x*x/4.0;
+		ans=(log(x/2.0)*besi1(x))+(1.0/x)*(1.0+y*(0.15443144
+			+y*(-0.67278579+y*(-0.18156897+y*(-0.1919402e-1
+			+y*(-0.110404e-2+y*(-0.4686e-4)))))));
+	} else {
+		y=2.0/x;
+		ans=(exp(-x)/sqrt(x))*(1.25331414+y*(0.23498619
+			+y*(-0.3655620e-1+y*(0.1504268e-1+y*(-0.780353e-2
+			+y*(0.325614e-2+y*(-0.68245e-3)))))));
+	}
+	return ans;
+}
+
+double lk::gammln(double xx)
+{
+	double x,y,tmp,ser;
+	static double cof[6]={76.18009172947146,-86.50532032941677,
+		24.01409824083091,-1.231739572450155,
+		0.1208650973866179e-2,-0.5395239384953e-5};
+	int j;
+
+	y=x=xx;
+	tmp=x+5.5;
+	tmp -= (x+0.5)*log(tmp);
+	ser=1.000000000190015;
+	for (j=0;j<=5;j++) ser += cof[j]/++y;
+	return -tmp+log(2.5066282746310005*ser/x);
+}
+
+
+double lk::betacf(double a, double b, double x) throw( lk::error_t )
+{
+#define MAXIT 100
+#define EPS 3.0e-7
+#define FPMIN 1.0e-30
+
+	int m,m2;
+	double aa,c,d,del,h,qab,qam,qap;
+
+	qab=a+b;
+	qap=a+1.0;
+	qam=a-1.0;
+	c=1.0;
+	d=1.0-qab*x/qap;
+	if (fabs(d) < FPMIN) d=FPMIN;
+	d=1.0/d;
+	h=d;
+	for (m=1;m<=MAXIT;m++) {
+		m2=2*m;
+		aa=m*(b-m)*x/((qam+m2)*(a+m2));
+		d=1.0+aa*d;
+		if (fabs(d) < FPMIN) d=FPMIN;
+		c=1.0+aa/c;
+		if (fabs(c) < FPMIN) c=FPMIN;
+		d=1.0/d;
+		h *= d*c;
+		aa = -(a+m)*(qab+m)*x/((a+m2)*(qap+m2));
+		d=1.0+aa*d;
+		if (fabs(d) < FPMIN) d=FPMIN;
+		c=1.0+aa/c;
+		if (fabs(c) < FPMIN) c=FPMIN;
+		d=1.0/d;
+		del=d*c;
+		h *= del;
+		if (fabs(del-1.0) < EPS) break;
+	}
+	
+	if (m > MAXIT) throw lk::error_t("a or b too big, or MAXIT too small in betacf");
+	return h;
+	
+#undef MAXIT
+#undef EPS
+#undef FPMIN
+}
+
+double lk::betai(double a, double b, double x) throw( lk::error_t )
+{
+	void nrerror(char error_text[]);
+	double bt;
+
+	if (x < 0.0 || x > 1.0) throw lk::error_t("Bad x in routine betai");
+	if (x == 0.0 || x == 1.0) bt=0.0;
+	else
+		bt=exp(gammln(a+b)-gammln(a)-gammln(b)+a*log(x)+b*log(1.0-x));
+	if (x < (a+1.0)/(a+b+2.0))
+		return bt*betacf(a,b,x)/a;
+	else
+		return 1.0-bt*betacf(b,a,1.0-x)/b;
+}
+
+double lk::pearson( double *x, double *y, size_t len )
+{
+	// apd: implemented june 2012, from  http://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
+	// matches output of excel's CORREL function.
+
+	double xm = 0, ym = 0;
+	for (size_t i=0;i<len;i++)
+	{
+		xm += x[i];
+		ym += y[i];
+	}
+	xm /= len;
+	ym /= len;
+
+	double num = 0;
+	double xden = 0;
+	double yden = 0;
+
+	for (size_t i=0;i<len;i++)
+	{
+		num += (x[i]-xm)*(y[i]-ym);
+		xden += (x[i]-xm)*(x[i]-xm);
+		yden += (y[i]-ym)*(y[i]-ym);
+	}
+
+	return num / sqrt(xden*yden);
+}
+
+void lk::gser(double *gamser, double a, double x, double *gln)
+{
+#define ITMAX 100
+#define EPS 3.0e-7
+	int n;
+	double sum,del,ap;
+
+	*gln=gammln(a);
+	if (x <= 0.0) {
+		if (x < 0.0) throw lk::error_t("x less than 0 in routine gser");
+		*gamser=0.0;
+		return;
+	} else {
+		ap=a;
+		del=sum=1.0/a;
+		for (n=1;n<=ITMAX;n++) {
+			++ap;
+			del *= x/ap;
+			sum += del;
+			if (fabs(del) < fabs(sum)*EPS) {
+				*gamser=sum*exp(-x+a*log(x)-(*gln));
+				return;
+			}
+		}
+		throw lk::error_t("a too large, ITMAX too small in routine gser");
+		return;
+	}
+#undef ITMAX
+#undef EPS
+}
+
+
+void lk::gcf(double *gammcf, double a, double x, double *gln)
+{
+#define ITMAX 100
+#define EPS 3.0e-7
+#define FPMIN 1.0e-30
+	double gammln(double xx);
+	void nrerror(char error_text[]);
+	int i;
+	double an,b,c,d,del,h;
+
+	*gln=gammln(a);
+	b=x+1.0-a;
+	c=1.0/FPMIN;
+	d=1.0/b;
+	h=d;
+	for (i=1;i<=ITMAX;i++) {
+		an = -i*(i-a);
+		b += 2.0;
+		d=an*d+b;
+		if (fabs(d) < FPMIN) d=FPMIN;
+		c=b+an/c;
+		if (fabs(c) < FPMIN) c=FPMIN;
+		d=1.0/d;
+		del=d*c;
+		h *= del;
+		if (fabs(del-1.0) < EPS) break;
+	}
+	if (i > ITMAX) throw lk::error_t("a too large, ITMAX too small in gcf");
+	*gammcf=exp(-x+a*log(x)-(*gln))*h;
+#undef ITMAX
+#undef EPS
+#undef FPMIN
+}
+
+double lk::gammp(double a, double x)
+{
+	double gamser,gammcf,gln;
+
+	if (x < 0.0 || a <= 0.0) throw lk::error_t("Invalid arguments in routine gammp");
+	if (x < (a+1.0)) {
+		gser(&gamser,a,x,&gln);
+		return gamser;
+	} else {
+		gcf(&gammcf,a,x,&gln);
+		return 1.0-gammcf;
+	}
+}
+
+double lk::gammq(double a, double x)
+{
+	double gamser,gammcf,gln;
+
+	if (x < 0.0 || a <= 0.0) throw lk::error_t("Invalid arguments in routine gammq");
+	if (x < (a+1.0)) {
+		gser(&gamser,a,x,&gln);
+		return 1.0-gamser;
+	} else {
+		gcf(&gammcf,a,x,&gln);
+		return gammcf;
+	}
+}
+
+double lk::erf(double x)
+{
+	return x < 0.0 ? -gammp(0.5,x*x) : gammp(0.5,x*x);
+}
+double lk::erfc(double x)
+{
+	return x < 0.0 ? 1.0+gammp(0.5,x*x) : gammq(0.5,x*x);
+}
+
 
 #ifdef WIN32
 
--- lk_stdlib.h
+++ lk_stdlib.h
@@ -67,7 +67,28 @@ namespace lk {
 	lk_string format_vl( const lk_string &fmt, const std::vector< vardata_t* > &args );
 	lk_string format(const char *fmt, ...);
 	size_t format_vn(char *buffer, int maxlen, const char *fmt, va_list arglist);
+	
+	double besj0(double x);
+	double besj1(double x);
+	double besy0(double x);
+	double besy1(double x);
+	double besi0(double x);
+	double besk0(double x);
+	double besi1(double x);
+	double besk1(double x);
 
+	double gammln(double xx);
+	double betacf(double a, double b, double x) throw(lk::error_t);
+	double betai(double a, double b, double x) throw( lk::error_t );
+
+	double pearson(double *x, double *y, size_t len );
+
+	void gser(double *gamser, double a, double x, double *gln);
+	void gcf(double *gammcf, double a, double x, double *gln);
+	double gammp(double a, double x);
+	double gammq(double a, double x);
+	double erf(double x);
+	double erfc(double x);
 };
 
 
