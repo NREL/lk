d4824b9568fb439247cd59906a51e63d547f5b6b
sjanzou <steven.janzou@nrel.gov>
2017-09-25 05:26:49 -0600

    Various threading configuration testing

--- build_vc2013/test_thread/Part9.cpp
+++ build_vc2013/test_thread/Part9.cpp
@@ -11,7 +11,10 @@ using namespace std::chrono;
 std::string fetchDataFromDB(std::string recvdData)
 {
 	// Make sure that function takes 5 seconds to complete
-	std::this_thread::sleep_for(seconds(5));
+	//std::this_thread::sleep_for(seconds(5));
+	double x = 0;
+	for (size_t i=0;i<100000000; i++)
+		x=sin(i*0.4)+cos(i*0.5);
  
 	//Do stuff like creating DB Connection and fetching Data
 	return "DB_" + recvdData;
@@ -26,38 +29,46 @@ std::string fetchDataFromFile(std::string recvdData)
 	return "File_" + recvdData;
 }
  
-int main()
+int main_9()
 {
 	// Get Start Time
 	system_clock::time_point start = system_clock::now();
- 
 	std::cout << "spawn 1 " << std::endl;
-	std::future<std::string> resultFromDB = std::async(std::launch::async, fetchDataFromDB, "Data");
+	std::future<std::string> resultFromDB1 = std::async(std::launch::async, fetchDataFromDB, "Data1");
 	std::cout << "spawn 2 " << std::endl;
  	std::future<std::string> resultFromDB2 = std::async(std::launch::async, fetchDataFromDB, "Data2");
- 
- 
-	//Fetch Data from File
-//	std::string fileData = fetchDataFromFile("Data");
- 
+	std::cout << "spawn 3 " << std::endl;
+	std::future<std::string> resultFromDB3 = std::async(std::launch::async, fetchDataFromDB, "Data3");
+	std::cout << "spawn 4 " << std::endl;
+ 	std::future<std::string> resultFromDB4 = std::async(std::launch::async, fetchDataFromDB, "Data4");
 	//Fetch Data from DB
 	// Will block till data is available in future<std::string> object.
-	std::string dbData = resultFromDB.get();
+	std::string dbData1 = resultFromDB1.get();
 	std::cout << "get 1 " << std::endl;
 	std::string dbData2 = resultFromDB2.get();
 	std::cout << "get 2 " << std::endl;
- 
+	std::string dbData3 = resultFromDB3.get();
+	std::cout << "get 3 " << std::endl;
+	std::string dbData4 = resultFromDB4.get();
+	std::cout << "get 4 " << std::endl;
 	// Get End Time
 	auto end = system_clock::now();
- 
 	auto diff = duration_cast < std::chrono::seconds > (end - start).count();
 	std::cout << "Total Time Taken = " << diff << " Seconds" << std::endl;
- 
 	//Combine The Data
-	std::string data = dbData + " :: " + dbData2 ;//+ " :: " + fileData;
- 
+	std::string data = dbData1 + " :: " + dbData2  + " :: " +  dbData3 + " :: " + dbData4 ;//+ " :: " + fileData;
 	//Printing the combined Data
 	std::cout << "Data = " << data << std::endl;
- 
+	// sequential call comparison
+	start = system_clock::now();
+	dbData1 = fetchDataFromDB("sData1");
+	dbData2 = fetchDataFromDB("sData2");
+	dbData3 = fetchDataFromDB("sData3");
+	dbData4 = fetchDataFromDB("sData4");
+	end = system_clock::now();
+	diff = duration_cast < std::chrono::seconds > (end - start).count();
+	std::cout << "Sequntial Total Time Taken = " << diff << " Seconds" << std::endl;
+	data = dbData1 + " :: " + dbData2  + " :: " +  dbData3 + " :: " + dbData4 ;//+ " :: " + fileData;
+ 	std::cout << "Data = " << data << std::endl;
 	return 0;
 }
