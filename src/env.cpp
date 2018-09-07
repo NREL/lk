4579e73818bbb77d8db78401140a1cc05b77fe2f
Darice <darice.guittet@nrel.gov>
2018-09-07 13:12:02 -0600

    display fractions with more than 5 leading zeroes in scientific notation

--- src/env.cpp
+++ src/env.cpp
@@ -123,9 +123,11 @@ lk_string lk::vardata_t::as_string() const
 		char buf[512];
 		if (((double)((int)m_u.v)) == m_u.v)
 			sprintf(buf, "%d", (int)m_u.v);
+		else if (abs(m_u.v) < 1e-5 ) {
+			sprintf(buf, "%e", m_u.v);
+		}
 		else
 			sprintf(buf, "%lf", m_u.v);
-//		sprintf(buf, "%lg", m_u.v);
 		return lk_string(buf);
 	}
 	case STRING:
